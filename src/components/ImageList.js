1a6267e31ed596bd30c2cc09baaf73ba2de4f25c
Beata <beata.czekaj.7@gmail.com>
2020-08-30 21:57:47 +0200

    ImageList component cleanup

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => <img src={image.urls.regular} />);
+  const images = props.images.map(({ description, id, urls }) => (
+    <img alt={description} key={id} src={urls.regular} />
+  ));
   return <div>{images}</div>;
 };
 
